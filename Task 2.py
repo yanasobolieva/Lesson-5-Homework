924283c8959084760552c9a3f8908a57017e0356
yanasobolieva <soboleva.yana.sys@gmail.com>
2019-11-03 11:16:08 +0200

    Added assert to second task

--- Task 2.py
+++ Task 2.py
@@ -1,4 +1,7 @@
 # Returning unique ordered list
+from typing import List, Union, Tuple
+
+
 def unique_ordered(some_list: list):
     unique_ordered_list = []
     i = 0
@@ -15,9 +18,7 @@ print(unique_ordered(["a", 5, 2, 5, (1, "a"), "a"]) == ["a", 5, 2, (1, "a")])
 
 # Returning unique disordered list
 def unique_disordered(some_list: list):
-    temp_set = set(some_list)
-    unique_list = list(temp_set)
-    return unique_list
+    return list(set(some_list))
 
 
 # uncomment the nest line to see what the function returns
@@ -30,3 +31,7 @@ assert \
     len(set(unique_disordered(["a", 5, 2, 5, (1, "a"), "a"]))) == len(unique_disordered(["a", 5, 2, 5, (1, "a"), "a"]))
 print(
     len(set(unique_disordered(["a", 5, 2, 5, (1, "a"), "a"]))) == len(unique_disordered(["a", 5, 2, 5, (1, "a"), "a"])))
+# or using sorted in-build function:
+assert sorted((unique_disordered(["a", 5, 2, 5, (1, "a"), "a"])), key=str) == sorted([2, "a", 5, (1, "a")], key=str)
+print(sorted((unique_disordered(["a", 5, 2, 5, (1, "a"), "a"])), key=str) == sorted([2, "a", 5, (1, "a")], key=str))
+
