1ba87f3dff4c0fb2c611c2f82daef6c6d447e28c
yanasobolieva <soboleva.yana.sys@gmail.com>
2019-11-03 10:58:31 +0200

    Added one more approach

--- Task 3.py
+++ Task 3.py
@@ -14,3 +14,18 @@ def thirds_second_approach(some_tuple: tuple):
 
 assert thirds_second_approach((1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c')) == (3, 6, 9, 'b')
 print(thirds_second_approach((1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c')) == (3, 6, 9, 'b'))
+
+
+# Third approach using while:
+def thirds_third_approach(some_tuple: tuple):
+    index = 0
+    result = []
+    while index < len(some_tuple):
+        if (index + 1) % 3 == 0:
+            result.append(some_tuple[index])
+        index += 1
+    return tuple(result)
+
+
+assert thirds_third_approach((1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c')) == (3, 6, 9, 'b')
+print(thirds_third_approach((1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c')) == (3, 6, 9, 'b'))
